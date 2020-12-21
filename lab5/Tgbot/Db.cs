61453514e7b24d90315f6756f6be273b172eb293
andrii.diak <andrii.diak@avenga.com>
2020-12-21 11:08:20 +0200

    add lab 5

---
+++ lab5/Tgbot/Db.cs
@@ -0,0 +1,141 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace TGBot
+{
+    public class Db
+    {
+        static string connStr = "server=mysql.local;user=root;database=anime_db;port=3306;password=270998";
+        MySqlConnection connection;
+
+
+        public async Task ConnectAsync()
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                try
+                {
+                    connection = new MySqlConnection(connStr);
+                    Console.WriteLine($"Connecting to MySQL... ({i})");
+                    await connection.OpenAsync();
+                }
+                catch (Exception ex)
+                {
+                    connection.Dispose();
+                    connection = null;
+                    Console.WriteLine(ex.ToString());
+                    await Task.Delay(2000);
+                } 
+            }
+        }
+
+        public string cutName(string name)
+        {
+            char[] spearator = { '[' };
+            string[] cname = name.Split(spearator, 2, StringSplitOptions.None);
+            //Console.WriteLine(cname[0]);
+            return cname[0];
+        }
+        //public static string cutEpisode(string name)
+        //{
+        //    char[] spearator = { '[' };
+        //    string[] cname = name.Split(spearator, 2, StringSplitOptions.None);
+        //    //Console.WriteLine(cname[0]);
+        //    return cname[1];
+        //}
+        public string cutDate(string date)
+        {
+            char[] spearator = { ',', '+' };
+            string[] cname = date.Split(spearator, 3, StringSplitOptions.None);
+            return cname[1];
+        }
+        public void createList(long id, string ListAnimeId)
+        {
+            Console.WriteLine("id" + id);
+            String[] idAnime = ListAnimeId.Substring(6).Split(' ');
+
+            try
+            {
+                string sql = $"SELECT idanime FROM anime WHERE idanime IN (" +
+                    $"SELECT id_anime FROM user_anime  WHERE id_user = '{id}')";
+                MySqlCommand cmd = new MySqlCommand(sql, connection);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    String List = "";
+                    List += rdr[0];
+                    idAnime = idAnime.Where(val => val != List).ToArray();
+                }
+                rdr.Close();
+                for (int i = 0; i < idAnime.Length; i++)
+                {
+                    sql = $"INSERT INTO user_anime (id_user,id_anime) VALUES ('{id}', '{idAnime[i]}')";
+                    cmd = new MySqlCommand(sql, connection);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            Console.WriteLine("Done.");
+        }
+
+        public string printList(long id)
+        {
+            String List = " ";
+            try
+            {
+                String sql = $"SELECT * FROM anime WHERE idanime IN (" +
+                    $"SELECT id_anime FROM user_anime  WHERE id_user = '{id}')";
+                MySqlCommand cmd = new MySqlCommand(sql, connection);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    List += rdr[0] + " -- " + rdr[1] + " -- " + rdr[2] + "\n";
+                }
+                rdr.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("Done.");
+            }
+            return List;
+        }
+
+        public string printAllList()
+        {
+            String List = " ";
+            try
+            {
+                String sql = "SELECT * FROM anime ";
+                MySqlCommand cmd = new MySqlCommand(sql, connection);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    List += rdr[0] + " -- " + rdr[1] + " -- " + rdr[2] + "\n";
+                }
+                rdr.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("Done.");
+            }
+            return List;
+        }
+    }
+}
---
+++ lab5/Tgbot/Telegram.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types.Enums;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using System.Threading;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TGBot
+{
+    public class TelegramService : IHostedService
+    {
+        private static TelegramBotClient client;
+        private static Db db;
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            db = new Db();
+            await db.ConnectAsync();
+
+            client = new TelegramBotClient("1305495277:AAGlAxNzukQ190g0ysHZtSLYhbQx5XbIplk");
+            client.OnMessage += BotOnMessageReceived;
+            client.OnMessageEdited += BotOnMessageReceived;
+            client.StartReceiving();
+
+            Console.WriteLine("Server has been started...");
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            client.OnMessage -= BotOnMessageReceived;
+            client.OnMessageEdited -= BotOnMessageReceived;
+            client.StopReceiving();
+
+            return Task.CompletedTask;
+        }
+
+        private static async void BotOnMessageReceived(object sender, MessageEventArgs e)
+        {
+            var message = e.Message;
+            if (e.Message.Text == "/command1")
+            {
+                await client.SendTextMessageAsync(message.Chat.Id, db.printAllList());
+                await client.SendTextMessageAsync(message.Chat.Id, "enter id anime\n list: 1 2 3");
+            }
+            if (e.Message.Text.Contains("list:"))
+            {
+                db.createList(message.Chat.Id, e.Message.Text);
+                await client.SendTextMessageAsync(message.Chat.Id, "correct");
+            }
+            if (e.Message.Text == "/command2")
+            {
+                await client.SendTextMessageAsync(message.Chat.Id, db.printAllList());
+
+            }
+            if (e.Message.Text == "/command3")
+            {
+                await client.SendTextMessageAsync(message.Chat.Id, db.printList(message.Chat.Id));
+
+            }
+
+            if (e.Message.Text == "hi")
+            {
+                await client.SendTextMessageAsync(message.Chat.Id, message.Text);
+                //await client.SendStickerAsync(message.Chat.Id, "CAACAgIAAxkBAAI_r1 -Ybt8xEUS0AAFBldDnEkgi2IYUZAACDSMAAuCjggeXsvhpxp-R4xsE");
+                await client.SendTextMessageAsync(message.Chat.Id, @"https://animevost.org/page/5/");
+                await client.SendPhotoAsync(message.Chat.Id, @"https://i.pinimg.com/564x/40/90/cf/4090cf733c863ac75d7c545bdb25b39e.jpg");
+            }
+            if (e.Message.Text == "how are you?")
+            {
+                await client.SendTextMessageAsync(message.Chat.Id, "Fine");
+                await client.SendStickerAsync(message.Chat.Id, "CAACAgIAAxkBAAI_u1-YdLryTelO5qHopBrcbIPuinU3AAIaIwAC4KOCB3IDQ3fHtpa2GwQ");
+            }
+            if (message?.Type == MessageType.Sticker)
+            {
+                await client.SendPhotoAsync(message.Chat.Id, @"https://i.pinimg.com/originals/f4/d2/96/f4d2961b652880be432fb9580891ed62.png");
+            }
+        }
+    }
+
+    class Telegram
+    {
+        static async Task Main(string[] args)
+        {
+            var hostBuilder = new HostBuilder()
+                .ConfigureServices(services =>
+                {
+                    services.AddHostedService<TelegramService>();
+                });
+
+            await hostBuilder.RunConsoleAsync();
+        }
+    }
+}
